0d3048fb6e5298920d91e8378fed593c8ed9329f
Carlos Ferreira <108193136+CarlosFerreiraJr@users.noreply.github.com>
2022-06-25 19:41:48 -0300

    Add files via upload

---
+++ exemplo_02.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("DataFrame").getOrCreate()
+
+df = spark.read.text("dados.txt")
+
+df.selectExpr("split(value, ';') as\
+Text_Data_In_Rows_Using_Text").show(4, False)
