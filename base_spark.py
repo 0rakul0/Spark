c8c25c601bda6d8109bd37a8a59fb66e526a9369
ANJOS, J. S <0rakul0render@gmail.com>
2022-06-25 14:40:37 -0300

    spark no pycharm
    aplicação pratica em py arquivo base_spark

--- base_spark.py
+++ base_spark.py
@@ -1,2 +1,32 @@
-from pyspark.sql.functions import max, desc, asc, expr
+# import pyspark class Row from module sql
+from pyspark.shell import spark
+from pyspark.sql.functions import max, desc, asc, expr, min
+from pyspark.sql import *
 
+class spark_big_data():
+
+    def run(self, arquivo):
+        schema = self.schema(arquivo)
+        manipulacao = self.manipulacao(schema)
+
+    def schema(self, arquivo):
+        arquivo = arquivo
+        df = spark.read.format("csv").option("inferSchema", "True").option("header", "True").csv(arquivo)
+        df.printSchema()
+        return df
+
+    def manipulacao(self, schema):
+        try:
+            valor_max = schema.select(max("valor")).take(1)
+            valor_min = schema.select(min("valor")).take(1)
+
+            print(valor_max)
+            print(valor_min)
+        except Exception as e:
+            print(e)
+
+if __name__=="__main__":
+    sp = spark_big_data()
+
+    end = "./data/cotacaoValores.csv"
+    sp.run(arquivo=end)
