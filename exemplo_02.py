d37a13d321b8fff2c2e05165fc4bdab4da5c6ed8
ANJOS, J. S <0rakul0render@gmail.com>
2022-06-25 20:21:02 -0300

    add data_valores para exemoplo_02

--- exemplo_02.py
+++ exemplo_02.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("DataFrame").getOrCreate()
 
-df = spark.read.text("dados.txt")
+df = spark.read.text("./data/cotacaoValores.txt")
 
 df.selectExpr("split(value, ';') as\
-Text_Data_In_Rows_Using_Text").show(4, False)
+Text_Data_In_Rows_Using_Text").show(5, False)
