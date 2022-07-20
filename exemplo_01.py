00d49b25666357bd3365806ec92ff3832975fbed
ANJOS, J. S <0rakul0render@gmail.com>
2022-07-19 21:00:10 -0300

    att

--- exemplo_01.py
+++ exemplo_01.py
@@ -7,6 +7,7 @@ import spark_df_profiling
 from pyspark.sql import *
 from wordcloud import WordCloud, STOPWORDS
 from operator import add
+import matplotlib.pyplot as plt
 from pyspark.mllib.classification import NaiveBayes, NaiveBayesModel
 from pyspark.mllib.util import MLUtils
 
@@ -14,12 +15,13 @@ class spark_big_data():
     def __init__(self):
         self.rdd = None
 
-    def run(self, arquivo, rdd=None, info=None, view=None, view_unique_column=None, profiling=None, cloud=None, search=None, analise=None):
-        schema = self.schema(arquivo, rdd, spark)
+    def run(self, arquivo, rdd=None, info=None, view=None, view_unique_column=None, profiling=None, cloud=None, search=None, analise=None, sql=None,
+            nome_schema=None):
+        schema = self.schema(arquivo, rdd, spark, nome_schema)
         if info:
             self.info_schema(schema)
         if view:
-            self.view_schema(schema)
+            self.view_schema(schema, sql)
         if profiling:
             self.profilling(schema)
         if view_unique_column:
@@ -30,10 +32,10 @@ class spark_big_data():
             self.search(schema, search)
         if analise:
             self.contador(schema)
-        schema.cache()
+
         return schema
 
-    def schema(self, arquivo, rdd, spark):
+    def schema(self, arquivo, rdd, spark, nome_schema):
         arquivo = arquivo
 
         if rdd:
@@ -44,9 +46,12 @@ class spark_big_data():
             schema = df.select("*").withColumn("id", monotonically_increasing_id())
             schema.cache()
         else:
-            spark = SparkSession.builder.appName("Schema_twitter").getOrCreate()
-            schema = spark.read.option("header", "True").csv(arquivo)
-
+            #gera uma sessão
+            sparkSession = SparkSession.builder.appName("Schema_twitter").getOrCreate()
+            # gera um dataframe com o melhor do drr e dataframe
+            schema = sparkSession.read.option("header", "True").option("sep", ";").csv(arquivo)
+            schema.createTempView(f'{nome_schema}')
+            return schema
         self.rdd = rdd
         return schema
 
@@ -54,12 +59,14 @@ class spark_big_data():
         # informações do tipo de dados do banco
         schema.printSchema()
 
-    def view_schema(self, schema):
+    def view_schema(self, schema, sql):
         #toda a tabela
         if self.rdd==False:
             schema.select("id", "tweet_text", "tweet_date", "sentiment", "query_used").show()
-        else:
+        elif self.rdd==False:
             print(schema.collect())
+        elif sql:
+            spark.sql(f'{sql}').show()
 
     def profilling(self, schema):
         schema.printSchema()
@@ -90,7 +97,7 @@ class spark_big_data():
         else:
             texto = schema.select("id", "tweet_text", "sentiment", "query_used")
             for i in texto.collect():
-                if re.search(f'{search}', str(i), re.IGNORECASE):
+                if re.search(f'\w{search}$', str(i), re.IGNORECASE):
                     print(f'\nA palavra pesquisada foi {search} no tweet {i}')
 
     def contador(self, schema):
@@ -105,13 +112,22 @@ class spark_big_data():
             #aplicação de regex
             totalSentimentoPositivos = schema.filter(col("sentiment").rlike('1')).count()
             totalSentimentoNegativos = schema.filter(col("sentiment").rlike('0')).count()
-
             print(f"sentimentos positivos:{totalSentimentoPositivos}, sentimentos negativos: {totalSentimentoNegativos}")
 
 
 if __name__ == "__main__":
     sp = spark_big_data()
     end = "./data/Test.csv"
-    df = sp.run(arquivo=end, rdd=True, analise=True, search='bolsonaro')
 
-    #com o rdd  = False pega mais coisa
+    # nota só usar um metodo de >> | sql | rdd | dataframe |
+    nome_schema = "schema_twitter"
+    #analise com sql
+    sql = f"select count(sentiment) as qte, sentiment from {nome_schema} group by (sentiment)"
+    df = sp.run(arquivo=end, view=True, sql=sql, nome_schema=nome_schema)
+    
+
+    # com analise ativa
+    # df = sp.run(arquivo=end, analise=True)
+
+    #view
+    # df = sp.run(arquivo=end, analise=True)
